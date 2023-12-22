ab8f8e7cc9f7c2ff8d6392c119f70f701aa56743
suphattraying <suphattra.spk@gmail.com>
2023-12-22 17:25:28 +0700

    finish

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,28 +1,53 @@
+import { useState } from "react";
+import allPosts from "./AllPost";
+
+
 function Posts() {
+  const [postContent, setpost] = useState(allPosts);
+
+
+  const handleLikeClick = (index) => {
+    let newLikes = [...postContent]
+    newLikes[index].likes = newLikes[index].likes +1
+    setpost(newLikes)}
+
+  const handleDislikeClick = (index) => {
+    let newLikes = [...postContent]
+    if(newLikes[index].likes > 0){   
+        newLikes[index].likes = newLikes[index].likes -1
+        setpost(newLikes)
+    }
+    
+  };
+
+  
+
   return (
     <div class="app-wrapper">
       <h1 class="app-title">Posts</h1>
       <div class="post-list">
-        <div class="post-item">
+      {postContent.map((item,index) => {
+        return (
+          <div key ={index} class="post-item">
           <div class="post-header">
-            <h2>Post Title #1</h2>
+            <h2>{item.title} #{item.id}</h2>
             <div class="post-social-media-stats">
               <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
+              <span class="post-likes">{item.likes}</span>
             </div>
           </div>
           <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
+            {item.content}
           </p>
           <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
+            <button class="like-button " onClick={() => {handleLikeClick(index)}} >
+              Like
+            </button>
+            <button class="dislike-button" onClick={() => {handleDislikeClick(index)}} >Dislike</button>
           </div>
         </div>
+        )
+      })}     
       </div>
     </div>
   );
